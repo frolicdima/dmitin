5dc82bbd1d6eb2454675c3cc432b3a50da228dd7
Dmitrii Mitin <ppdnk@mail.ru>
2017-06-17 10:58:08 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,20 @@
 package ru.job4j;
 
+/**
+* Class Класс для чего-то
+* @author Dmitrii Mitin
+* @since 17.06.2017
+* @version 1
+*/
+
 public class Calculate {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+/**
+* Метод, вывод строки в консоль.
+* @param arg - arg.
+*/
+
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitrii Mitin (mailto:ppdnk@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
